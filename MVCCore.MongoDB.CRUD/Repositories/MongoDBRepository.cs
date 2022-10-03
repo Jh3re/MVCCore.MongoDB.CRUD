52f6d3fe934b3e0a24cb79ed2a2432a2f99b362e
Jheremy <chamy2406@gmail.com>
2022-10-03 09:46:35 -0400

    Conexion a MongoDB Atlas

--- MVCCore.MongoDB.CRUD/Repositories/MongoDBRepository.cs
+++ MVCCore.MongoDB.CRUD/Repositories/MongoDBRepository.cs
@@ -8,6 +8,8 @@ namespace MVCCore.MongoDB.CRUD.Repositories
         public IMongoDatabase db;
         public MongoDBRepository()
         {
+            // La cadena de conexion para usar MongoDB Atlas
+            // Mismo caso para conectar powershell, pero usamos la opciond de conectar a una aplicacion
             client = new MongoClient("mongodb+srv://jhere_1:mongodb2024@atlascluster.wlowxds.mongodb.net/?retryWrites=true&w=majority");
             db = client.GetDatabase("MusicCatalog");
         }
